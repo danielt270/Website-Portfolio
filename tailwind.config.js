1bd0a82c66e6eba3740d6edb960cf0037de1a795
danielt270 <dbt2700@gmail.com>
2024-05-09 16:33:00 -0400

    slowed animation speed

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         primaryDark: "#58E6D9", // 80,230,217
       },
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
+        'spin-slow': 'spin 18s linear infinite',
       }
     },
   },
