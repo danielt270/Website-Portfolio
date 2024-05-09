3d4a032b414b6f720b5ef145296a22c8f4baf285
danielt270 <dbt2700@gmail.com>
2024-05-09 16:18:05 -0400

    revised Hire Me sizing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import AnimatedText from '@/components/AnimatedText'
 import HireMe from '@/components/HireMe'
 import profilePic from '../../public/images/profile/developer-pic-1.png'
 import { LinkArrow } from '@/components/Icons'
+import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 
 export default function Home() {
   return (
@@ -40,6 +41,9 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
+        <div className='absolute right-8 bottom-5 inline-block w-24'>
+          <Image src={lightBulb} alt="DT" className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
