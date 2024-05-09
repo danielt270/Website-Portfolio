74ca8b7ba02af04d571e39bb3cb0698f271d3161
danielt270 <dbt2700@gmail.com>
2024-05-09 15:45:39 -0400

    added custom cyber-nuclear-student rounded text banner

---
+++ src/components/HireMe.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { CircularText, CustomBanner } from './Icons'
+
+const HireMe = () => {
+  return (
+    <div className='fixed left-4 bottom-4
+    flex items-center justify-center overflow-hidden
+    '>
+        <div className='w-48 h-auto flex items-center justify-center relative'>
+            <CustomBanner className={""}/>
+        </div>
+
+    </div>
+  )
+}
+
+export default HireMe
