a0659fc0a6eed245e933d635bc3fae09c201b8ef
danielt270 <dbt2700@gmail.com>
2024-05-09 19:45:29 -0400

    added backgroundImage:circularLight

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       fontFamily: {
         mont: ['var(--font-mont)', ...fontFamily.sans], //this is where the mont font is added into the tailwind lib
       },
-      colors:{
+      colors: {
         dark: "#1b1b1b",
         light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199
@@ -16,6 +16,9 @@ module.exports = {
       },
       animation: {
         'spin-slow': 'spin 18s linear infinite',
+      },
+      backgroundImage:{
+        circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px);',
       }
     },
   },
