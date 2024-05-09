75cf8411e9b58ebb34496102102b65b246886bd7
danielt270 <dbt2700@gmail.com>
2024-05-09 14:32:43 -0400

    added Resume and Contact buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import Layout from '@/components/Layout'
 import AnimatedText from '@/components/AnimatedText'
 import profilePic from '../../public/images/profile/developer-pic-1.png'
+import { LinkArrow } from '@/components/Icons'
 
 export default function Home() {
   return (
@@ -28,8 +29,11 @@ export default function Home() {
                 <Link href="/Daniel Thomas Resume 050624.pdf" target={"_blank"}
                 className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
                 hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark'
-                >Resume</Link>
-                <Link href="mailto:dbt2700@gmail.com">Contact</Link>
+                download={true}
+                >Resume<LinkArrow className={"w-6 ml-1"}/></Link>
+                <Link href="mailto:dbt2700@gmail.com" target={"_blank"}
+                className='ml-4 text-lg font-medium capitalize text-dark underline'
+                >Contact</Link>
               </div>
             </div>
           </div>
