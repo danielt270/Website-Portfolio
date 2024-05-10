b17e910fc0da288c0ee825df40115af44f360cc0
danielt270 <dbt2700@gmail.com>
2024-05-10 17:31:08 -0400

    created title and FeaturedArticle

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,8 +1,50 @@
+import AnimatedText from '@/components/AnimatedText'
+import Layout from '@/components/Layout'
 import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import article1 from '../../public/images/articles/pagination component in reactjs.jpg'
+
+const FeaturedArticle = ({img, title, time, summary, link}) => {
+    return(
+        <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+            <Link href={link} target='_blank' className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
+                <Image src={img} alt={title} className='w-full h-auto' />
+            </Link>
+            <Link href={link} target='_blank'>
+                <h2 className='capitalize text-2xl font-bold my-2'>{title}</h2>
+            </Link>
+            <p className='text-sm mb-2'>{summary}</p>
+            <span className='text-primary font-semibold'>{time}</span>
+        </li>
+    )
+}
 
 const articles = () => {
   return (
-    <div>articles</div>
+    <>
+        <Head>
+            <title>DT | Articles Page</title>
+            <meta name="description" content="description goes here..." />
+        </Head>
+        <main className='w-full mb-16 flex flex-col items-center justify-center ovrflow-hidden'>
+            <Layout className='pt-16'>
+                <AnimatedText text="Words Can Change The World!" className='mb-16' />
+                <ul className='grid grid-col-2 gap-16'>
+                    <FeaturedArticle 
+                    title="Build A Custom Pagination Component In Reactjs From Scratch"
+                    summary="Learn how to build a custom pagination component in ReactJS from scratch.  Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+                    time="9 min read"
+                    link="/"
+                    img={article1}
+                    />
+                    <li>Featured articles-2</li>
+                    <li>Featured articles-3</li>
+                </ul>
+            </Layout>
+        </main>
+    </>
   )
 }
 
