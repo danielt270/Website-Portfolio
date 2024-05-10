0f384d1a2569df7c3170750bca409cecb4d48502
danielt270 <dbt2700@gmail.com>
2024-05-10 02:06:45 -0400

    added animations to details and LiIcon.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,17 +1,24 @@
 import React, { useRef } from 'react'
 import { motion, useScroll } from 'framer-motion'
+import LiIcon from './LiIcon'
 
 const Details = ({position, company, companyLink, duration, address, work}) => {
+    const ref = useRef(null);
     return (
-        <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
-            <div>
+        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+            <LiIcon reference={ref} />
+            <motion.div
+            initial={{y:50}}
+            whileInView={{y:0}}
+            transition={{duration:0.5, type:"spring"}}
+            >
                 <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={companyLink}
                 target="_blank"
                 className="text-primary capitalize"
                 >@{company}</a></h3>
                 <span className='capitalize font-medium text-dark/75'>{duration} | {address}</span>
                 <p className='font-medium w-full'>{work}</p>
-            </div>
+            </motion.div>
         </li>
     )
 }
@@ -30,7 +37,7 @@ const Experience = () => {
     <div className='my-64'>
         <h2 className='font-bold text-8xl mb-32 w-full text-center'>Experience</h2>
         <div ref={ref} className='w-[75%] mx-auto relative'>
-            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top'/>
+            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/>
             <ul className='w-full flex flex-col items-start justify-between ml-4'>
                 <Details position="IT Intern" company="AMPAM Parks Mechanical" duration="Jun 2023 - Aug 2023" address="17036 Avalon Blvd, Carson, CA 90746"
                 companyLink="https://www.ampam.com" work="Assisted in assuring integrity of IT systems including computer infrastructure and software, 
