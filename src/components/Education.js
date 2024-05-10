f561ae0b978c626768d2efbe716be3970571d73c
danielt270 <dbt2700@gmail.com>
2024-05-10 02:24:32 -0400

    finished education

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,22 +2,19 @@ import React, { useRef } from 'react'
 import { motion, useScroll } from 'framer-motion'
 import LiIcon from './LiIcon'
 
-const Details = ({position, company, companyLink, duration, address, work}) => {
+const Details = ({type, duration, place, info}) => {
     const ref = useRef(null);
     return (
-        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col justify-between'>
             <LiIcon reference={ref} />
             <motion.div
             initial={{y:50}}
             whileInView={{y:0}}
             transition={{duration:0.5, type:"spring"}}
             >
-                <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={companyLink}
-                target="_blank"
-                className="text-primary capitalize"
-                >@{company}</a></h3>
-                <span className='capitalize font-medium text-dark/75'>{duration} | {address}</span>
-                <p className='font-medium w-full'>{work}</p>
+                <h3 className='capitalize font-bold text-2xl'>{type}</h3>
+                <span className='capitalize font-medium text-dark/75'>{duration} | {place}</span>
+                <p className='font-medium w-full'>{info}</p>
             </motion.div>
         </li>
     )
@@ -39,18 +36,31 @@ const Education = () => {
         <div ref={ref} className='w-[75%] mx-auto relative'>
             <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/>
             <ul className='w-full flex flex-col items-start justify-between ml-4'>
-                <Details position="IT Intern" company="AMPAM Parks Mechanical" duration="Jun 2023 - Aug 2023" address="17036 Avalon Blvd, Carson, CA 90746"
-                companyLink="https://www.ampam.com" work="Assisted in assuring integrity of IT systems including computer infrastructure and software, 
-                servers, firewalls, storage, operating system and virtualization"/>
-                <Details position="IT Intern" company="AMPAM Parks Mechanical" duration="Jun 2023 - Aug 2023" address="17036 Avalon Blvd, Carson, CA 90746"
-                companyLink="https://www.ampam.com" work="Assisted in assuring integrity of IT systems including computer infrastructure and software, 
-                servers, firewalls, storage, operating system and virtualization"/>
-                <Details position="IT Intern" company="AMPAM Parks Mechanical" duration="Jun 2023 - Aug 2023" address="17036 Avalon Blvd, Carson, CA 90746"
-                companyLink="https://www.ampam.com" work="Assisted in assuring integrity of IT systems including computer infrastructure and software, 
-                servers, firewalls, storage, operating system and virtualization"/>
-                <Details position="IT Intern" company="AMPAM Parks Mechanical" duration="Jun 2023 - Aug 2023" address="17036 Avalon Blvd, Carson, CA 90746"
-                companyLink="https://www.ampam.com" work="Assisted in assuring integrity of IT systems including computer infrastructure and software, 
-                servers, firewalls, storage, operating system and virtualization"/>
+                <Details 
+                type="B.S. Cybersecurity and Information Technology"
+                duration="2022-Present"
+                place="Purdue University"
+                info="Relevant courses include..." />
+                <Details 
+                type="B.S. Cybersecurity and Information Technology"
+                duration="2022-Present"
+                place="Purdue University"
+                info="Relevant courses include..." />
+                <Details 
+                type="B.S. Cybersecurity and Information Technology"
+                duration="2022-Present"
+                place="Purdue University"
+                info="Relevant courses include..." />
+                <Details 
+                type="B.S. Nuclear Engineering"
+                duration="2022-Present"
+                place="Purdue University"
+                info="Relevant courses include..." />
+                <Details 
+                type="B.S. ology"
+                duration="2022-Present"
+                place="Purdue University"
+                info="Relevant courses include..." />
             </ul>
         </div>
     </div>
