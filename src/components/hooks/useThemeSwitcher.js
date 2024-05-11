c930c0cd313c337956ea4fd8884a2870fd1fef3b
danielt270 <dbt2700@gmail.com>
2024-05-11 16:50:42 -0400

    finished ThemeSwitcher component

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const useThemeSwitcher = () => {
-  return (
-    <div>useThemeSwitcher</div>
-  )
+    const preferDarkQuery = "(prefer-coor-scheme: dark)";
+    const [mode, setMode] = useState("");
+    
+    //CHECKS IF THERE IS ANY USER SETTINGS/CONFIGURATION THROUGH LOCAL STORAGE
+    useEffect(() => {
+        const mediaQuery = window.matchMedia(preferDarkQuery);
+        const userPref = window.localStorage.getItem('theme')
+        
+        const handleChange = () => {
+            if(userPref) {
+                let check = userPref === 'dark' ? 'dark' : 'light';
+                setMode(check);
+                if(check === 'dark'){
+                    document.documentElement.classList.add('dark')
+                }
+                else {
+                    document.documentElement.classList.remove('dark')
+                }
+            }
+            else {
+                let check = mediaQuery.matches ? 'dark' : 'light';
+                setMode(check);
+
+                if(check === 'dark'){
+                    document.documentElement.classList.add('dark')
+                }
+                else {
+                    document.documentElement.classList.remove('dark')
+                }
+            }
+        }
+        mediaQuery.addEventListener('change', handleChange)
+
+        return () => mediaQuery.removeEventListener('change', handleChange)
+    }, [])
+
+    //UPDATES THE VALUE
+    useEffect(() => {
+        if(mode === 'dark'){
+            window.localStorage.setItem('theme','dark')
+            document.documentElement.classList.add('dark')
+        }
+        else {
+            window.localStorage.setItem('theme','light')
+            document.documentElement.classList.remove('dark')
+        }
+    }, [mode])
+
+    return [mode, setMode]
 }
 
 export default useThemeSwitcher
