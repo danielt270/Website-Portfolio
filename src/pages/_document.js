3bb42a42549d0523d9dcd2f4d00295a5a7b0c425
danielt270 <dbt2700@gmail.com>
2024-05-11 17:18:41 -0400

    fixed reload flash

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Script from 'next/script'
+//CUSTOM DOCUMENT THAT UPDATES THE HTML AND BODY TEXT THAT IS USED TO RENDER A PAGE
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+      <Script id='theme-swticher' strategy='beforeInteractive'>
+        {`
+          if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark')
+          } else {
+            document.documentElement.classList.remove('dark')
+          }
+        `}
+      </Script>
         <Main />
         <NextScript />
       </body>
