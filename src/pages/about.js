aba3565f692773f0f6a76ec3d3e6d110dc4c6798
danielt270 <dbt2700@gmail.com>
2024-05-13 16:29:43 -0400

    set dark mode for banner and skills

--- src/pages/about.js
+++ src/pages/about.js
@@ -45,7 +45,7 @@ const about = () => {
                 <AnimatedText text="Passion Fuels Purpose!" className='mb-16' />
                 <div className='grid w-full grid-cols-8 gap-16'>
                     <div className='col-span-3 flex flex-col items-start justify-start'>
-                        <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>About Me</h2>
+                        <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>About Me</h2>
                         <p className='font-medium'>
                             Hi, I'm blah blah blah
                         </p>            
@@ -57,7 +57,7 @@ const about = () => {
                         </p>
                     </div>
                     <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid 
-                    border-dark bg-light p-8'>
+                    border-dark bg-light p-8 dark:bg-dark dark:border-light'>
                         <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                         <Image src={profilePic} alt="DT" className='w-full h-auto rounded-2xl' />
                     </div>
