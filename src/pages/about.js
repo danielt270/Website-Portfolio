15262cb9e1ecb625cbc3db010dc1c534ae874d77
danielt270 <dbt2700@gmail.com>
2024-05-13 15:57:57 -0400

    added dark mode border for pfp

--- src/pages/about.js
+++ src/pages/about.js
@@ -58,7 +58,7 @@ const about = () => {
                     </div>
                     <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid 
                     border-dark bg-light p-8'>
-                        <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />
+                        <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                         <Image src={profilePic} alt="DT" className='w-full h-auto rounded-2xl' />
                     </div>
                     <div className='col-span-2 flex flex-col items-end justify-between'>
