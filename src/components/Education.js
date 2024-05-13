69a26c95977c107177b540912dbbe7eb93a0072d
danielt270 <dbt2700@gmail.com>
2024-05-13 16:42:40 -0400

    configured dark mode for education

--- src/components/Education.js
+++ src/components/Education.js
@@ -13,7 +13,7 @@ const Details = ({type, duration, place, info}) => {
             transition={{duration:0.5, type:"spring"}}
             >
                 <h3 className='capitalize font-bold text-2xl'>{type}</h3>
-                <span className='capitalize font-medium text-dark/75'>{duration} | {place}</span>
+                <span className='capitalize font-medium text-dark/75 dark:text-light/75'>{duration} | {place}</span>
                 <p className='font-medium w-full'>{info}</p>
             </motion.div>
         </li>
@@ -34,7 +34,7 @@ const Education = () => {
     <div className='my-64'>
         <h2 className='font-bold text-8xl mb-32 w-full text-center'>Education</h2>
         <div ref={ref} className='w-[75%] mx-auto relative'>
-            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/>
+            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light'/>
             <ul className='w-full flex flex-col items-start justify-between ml-4'>
                 <Details 
                 type="High School Diploma"
