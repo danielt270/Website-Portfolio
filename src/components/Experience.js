0ab9cf494a4eced72e2ecab709b73bf5743f5d49
danielt270 <dbt2700@gmail.com>
2024-05-13 16:40:45 -0400

    configured experience dark mode

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,11 +11,15 @@ const Details = ({position, company, companyLink, duration, address, work}) => {
             initial={{y:50}}
             whileInView={{y:0}}
             transition={{duration:0.5, type:"spring"}}>
-                <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={companyLink}
-                target="_blank"
-                className="text-primary capitalize"
-                >@{company}</a></h3>
-                <span className='capitalize font-medium text-dark/75'>{duration} | {address}</span>
+                <h3 className='capitalize font-bold text-2xl'>
+                    {position}&nbsp;
+                    <a href={companyLink}
+                    target="_blank"
+                    className="text-primary capitalize dark:text-primaryDark" >
+                        @{company}
+                    </a>
+                </h3>
+                <span className='capitalize font-medium text-dark/75 dark:text-light/75'>{duration} | {address}</span>
                 <p className='font-medium w-full'>{work}</p>
             </motion.div>
         </li>
@@ -36,7 +40,7 @@ const Experience = () => {
     <div className='my-64'>
         <h2 className='font-bold text-8xl mb-32 w-full text-center'>Experience</h2>
         <div ref={ref} className='w-[75%] mx-auto relative'>
-            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/>
+            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light'/>
             <ul className='w-full flex flex-col items-start justify-between ml-4'>
                 <Details position="IT Intern" company="AMPAM Parks Mechanical" duration="Jun 2023 - Aug 2023" address="17036 Avalon Blvd, Carson, CA 90746"
                 companyLink="https://www.ampam.com" work="Assisted in assuring integrity of IT systems including computer infrastructure and software, 
