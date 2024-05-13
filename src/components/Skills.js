ad71533d8126f3b7089c9c89e07d4ffc0d40f69b
danielt270 <dbt2700@gmail.com>
2024-05-13 16:35:20 -0400

    fixed slow hover for skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,8 +8,7 @@ const Skill = ({name, x, y}) => {
         py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light' 
         whileHover={{scale:1.05}}
         initial={{x:0,y:0}}
-        whileInView={{x:x,y:y}}
-        transition={{duration:1.5}}
+        whileInView={{x:x,y:y,transition:{duration:1.5}}}
         viewport={{once:true}}>
             {name}
         </motion.div>  
