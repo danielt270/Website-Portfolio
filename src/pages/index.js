42f61b03be8fe55f1d3b9252d97307f52064e94d
danielt270 <dbt2700@gmail.com>
2024-05-13 20:29:29 -0400

    made Resume and Contact responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,13 +33,14 @@ export default function Home() {
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="/Daniel Thomas Resume 050624.pdf" target={"_blank"}
-                className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
-                hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark 
-                dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light'
-                download={true}
+                 className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+                 hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark 
+                 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
+                 md:p-2 md:px-4 md:text-base'
+                 download={true}
                 >Resume<LinkArrow className={"w-6 ml-1"}/></Link>
                 <Link href="mailto:dbt2700@gmail.com" target={"_blank"}
-                className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light'
+                className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'
                 >Contact</Link>
               </div>
             </div>
