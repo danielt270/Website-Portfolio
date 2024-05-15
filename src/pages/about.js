0ebdf758905cdd68a80d9ff70fbc8fec39460d2c
danielt270 <dbt2700@gmail.com>
2024-05-14 23:32:10 -0400

    made counters responsive

--- src/pages/about.js
+++ src/pages/about.js
@@ -43,9 +43,9 @@ const about = () => {
         <main className='flex w-full flex-col items-center justify-center dark:text-light'>
             <Layout className='pt-16'>
                 <AnimatedText text="Passion Fuels Purpose!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
-                <div className='grid w-full grid-cols-8 gap-16'>
-                    <div className='col-span-3 flex flex-col items-start justify-start'>
-                        <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>About Me</h2>
+                <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+                    <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4'>
+                        <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75 '>About Me</h2>
                         <p className='font-medium'>
                             Hi, I'm blah blah blah
                         </p>            
@@ -57,13 +57,13 @@ const about = () => {
                         </p>
                     </div>
                     <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid 
-                    border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+                    border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4'>
                         <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                         <Image src={profilePic} alt="DT" className='w-full h-auto rounded-2xl' 
                         priority
                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
                     </div>
-                    <div className='col-span-2 flex flex-col items-end justify-between'>
+                    <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center'>
                         <div className='flex flex-col items-end justify-center'>
                             <span className='inline-block text-7xl font-bold'>
                                 <AnimatedNumbers value={40} /> +
