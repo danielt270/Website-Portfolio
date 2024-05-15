7dcf2397bfe73931ea986e7575fa1818055fb1e7
danielt270 <dbt2700@gmail.com>
2024-05-15 18:21:28 -0400

    made article responsive

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -33,7 +33,7 @@ const MovingImg = ({title, img, link}) => {
     return (
         <Link href={link} target='_blank' onMouseMove={handleMouse} onMouseLeave={handleMouseLeave}>
             <h2 className='capitalize text-xl font-semibold hover:underline'>{title}</h2>
-            <FramerImage ref={imgRef} src={img} alt={title} className='z-10 w-96 h-auto hidden absolute rounded-lg' 
+            <FramerImage ref={imgRef} src={img} alt={title} className='z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden' 
             style={{x:x, y:y}} 
             initial={{opacity:0}}
             whileInView={{opacity:1, transition:{duration: 0.2}}}
@@ -45,12 +45,12 @@ const MovingImg = ({title, img, link}) => {
 const Article = ({img, title, date, link}) => {
     return (
         <motion.li className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border 
-        border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light'
+        border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light sm:flex-col'
         initial={{y:200}}
         whileInView={{y:0,transition:{duration:0.5, ease:"easeInOut"}}}
         viewport={{once:true}}>
             <MovingImg title={title} img={img} link={link} />
-            <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>{date}</span>
+            <span className='text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm'>{date}</span>
         </motion.li>
     )
 
@@ -68,7 +68,7 @@ const FeaturedArticle = ({img, title, time, summary, link}) => {
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"/>
             </Link>
             <Link href={link} target='_blank'>
-                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline'>{title}</h2>
+                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline xs:text-lg'>{title}</h2>
                 
             </Link>
             <p className='text-sm mb-2'>{summary}</p>
@@ -86,8 +86,8 @@ const articles = () => {
         </Head>
         <main className='w-full mb-16 flex flex-col items-center justify-center ovrflow-hidden dark:text-light'>
             <Layout className='pt-16'>
-                <AnimatedText text="Words Can Change The World!" className='mb-16' />
-                <ul className='grid grid-cols-2 gap-16'>
+                <AnimatedText text="Words Can Change The World!" className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl' />
+                <ul className='grid grid-cols-2 gap-16 md:grid-cols-1 lg:gap-8 md:gap-y-16'>
                     <FeaturedArticle 
                 title="Build A Custom Pagination Component In Reactjs From Scratch"
                 summary="Learn how to build a custom pagination component in ReactJS from scratch.  Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
