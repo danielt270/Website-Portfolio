d7db54093e4d5af186557d38c8daa67239c8c225
danielt270 <dbt2700@gmail.com>
2024-05-15 00:06:58 -0400

    made education responsive

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,13 +5,13 @@ import LiIcon from './LiIcon'
 const Details = ({position, company, companyLink, duration, address, work}) => {
     const ref = useRef(null);
     return (
-        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col justify-between'>
+        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col justify-between items-center md:w-[80%]'>
             <LiIcon reference={ref} />
             <motion.div
             initial={{y:50}}
             whileInView={{y:0}}
             transition={{duration:0.5, type:"spring"}}>
-                <h3 className='capitalize font-bold text-2xl'>
+                <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>
                     {position}&nbsp;
                     <a href={companyLink}
                     target="_blank"
@@ -19,8 +19,10 @@ const Details = ({position, company, companyLink, duration, address, work}) => {
                         @{company}
                     </a>
                 </h3>
-                <span className='capitalize font-medium text-dark/75 dark:text-light/75'>{duration} | {address}</span>
-                <p className='font-medium w-full'>{work}</p>
+                <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>
+                    {duration} | {address}
+                </span>
+                <p className='font-medium w-full md:text-sm'>{work}</p>
             </motion.div>
         </li>
     )
@@ -38,10 +40,12 @@ const Experience = () => {
   )
   return (
     <div className='my-64'>
-        <h2 className='font-bold text-8xl mb-32 w-full text-center'>Experience</h2>
-        <div ref={ref} className='w-[75%] mx-auto relative'>
-            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light'/>
-            <ul className='w-full flex flex-col items-start justify-between ml-4'>
+        <h2 className='font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>Experience</h2>
+        <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
+            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top 
+             dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]'
+            />
+            <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
                 <Details position="IT Intern" company="AMPAM Parks Mechanical" duration="Jun 2023 - Aug 2023" address="17036 Avalon Blvd, Carson, CA 90746"
                 companyLink="https://www.ampam.com" work="Assisted in assuring integrity of IT systems including computer infrastructure and software, 
                 servers, firewalls, storage, operating system and virtualization"/>
