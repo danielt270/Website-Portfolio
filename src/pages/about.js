9a8926aa4358366689f0a4ceba4f28b6c09cb88f
danielt270 <dbt2700@gmail.com>
2024-05-15 23:08:28 -0400

    added transition effect to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -40,6 +40,7 @@ const about = () => {
             <title>DT | About Page</title>
             <meta name="description" content="description goes here..." />
         </Head>
+        <TransitionEffect />
         <main className='flex w-full flex-col items-center justify-center dark:text-light'>
             <Layout className='pt-16'>
                 <AnimatedText text="Passion Fuels Purpose!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
