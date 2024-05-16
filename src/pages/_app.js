a1a0d505e67ced0408e804d53b959f167883e652
danielt270 <dbt2700@gmail.com>
2024-05-15 23:20:38 -0400

    added animate presence

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google' //mont font pulled from google
 import Head from 'next/head'
 import Footer from '@/components/Footer'
+import { AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 //this is where the mont font is styled/instantialized
 const montserrat = Montserrat({
@@ -12,6 +14,8 @@ const montserrat = Montserrat({
 })
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  
   return (
     <>
       <Head>
@@ -20,7 +24,9 @@ export default function App({ Component, pageProps }) {
       </Head>
       <main className={'${montserrat.variable} font-mont bg-light w-full min-h-screen dark:bg-dark'}>
         <NavBar />
-        <Component {...pageProps} />
+        <AnimatePresence mode="wait">
+          <Component key={router.asPath} {...pageProps} />
+        </AnimatePresence>
         <Footer />
       </main>
     </>
